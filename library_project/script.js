96d3c39c942368cf9cde0e6813b60e97a5cf5379
elfivoid <elisa@aulweb.de>
2026-03-31 19:42:06 +0200

    Added Delete Script
    - created a function to delete book items
    - created book array

--- library_project/script.js
+++ library_project/script.js
@@ -1,9 +1,33 @@
-const myLibrary = [];
+//variables
+const CloseButtons = document.querySelectorAll(".close-btn");
 
-function Book() {
+// create new books
+const hobbit_book = new Book("The Hobbit", "J.R.R. Tolkien", 300, true);
+const alice_book = new Book("Alice in Wonderland","Lewis Carroll", 105, false);
 
+const myLibrary = [
+    hobbit_book,
+    alice_book
+];
+// create book constructor function
+function Book (title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
 function addBookToLibrary() {
 
 }
+function removeBook() {
+    CloseButtons.forEach(button => {
+        button.addEventListener("click", function(event) {
+            event.target.closest(".book").remove();
+        });
+    });
+   }
+
+   //call
+   removeBook();
+
